8732a5b1bc4652bb2573390a1a949a49cd39336c
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-10 14:22:45 +0000

    added upload middleware to create leagues route

--- routes/create_leagues.routes.js
+++ routes/create_leagues.routes.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const upload = require('../middleware/upload');
 var router = express.Router();
 
 /* GET users listing. */
@@ -6,9 +7,10 @@ router.get('/', function(req, res, next) {
     res.render('create-leagues', { title: 'Create League' });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', upload.single("league_logo"), function(req, res, next) {
     //TODO: Terryson: add new league record to the leagues table
     //write you code here.
+    let logo = req.file.filename
 });
 
 module.exports = router;
