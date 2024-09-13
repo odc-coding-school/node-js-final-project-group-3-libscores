42080267c68f96a7fb30cee8fa7581af4ba16620
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-13 15:43:22 +0000

    modified result page

--- routes/first.division.routes.js
+++ routes/first.division.routes.js
@@ -3,17 +3,30 @@ const upload = require('../middleware/upload');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-    console.log("working with nothing")
+router.get('/?', function(req, res, next) {
+    console.log(req)
+
     res.render('first-division', 
         { 
             title: 'Orange First Division League', 
             logo: "/images/league_1.png",
-            param: req.query,
             league: "l1",
             page: "first_division"
         }
     );
 });
+
+// router.get("/?", function(req, res, next) {
+//     console.log(req.query)
+//     res.render('first-division', 
+//         { 
+//             title: 'Orange First Division League', 
+//             logo: "/images/league_1.png",
+//             param: req.query,
+//             league: "l1",
+//             page: "first_division"
+//         }
+//     )
+// })
 
 module.exports = router;
