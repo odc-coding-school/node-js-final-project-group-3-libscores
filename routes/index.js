ddeec0757b1241ebf19a1068a51e6b12ed53d5d7
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-15 08:08:17 +0000

    get league one data from api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var cheerio = require("cheerio")
+var axios = require("axios")
+var allLeaguesApiUrl =""
+var app = require("../app")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LibScores', league: "/" });
+router.get('/', async function(req, res, next) {
+  let leagues = {}
+  let options = { title: 'libScores', league: "/" }
+  try {
+    await axios.get("https://www.thesportsdb.com//api/v1/json/3/search_all_leagues.php?c=Liberia")
+    .then(response => {
+      console.log(response.data.countries[0])
+      app.locals.league_1 = response.data.countries[0]
+    })
+    .catch(error => console.log(error))
+    res.render('index', options);
+  } catch (err) {
+    console.log(err)
+    options.err = err
+    res.render('index', options);
+  }
 });
 
 module.exports = router;
