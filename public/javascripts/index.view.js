0c356104395a07f9ce5082569614d1799e8f286d
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-15 12:06:32 +0000

    Added spinner

--- public/javascripts/index.view.js
+++ public/javascripts/index.view.js
@@ -3,17 +3,14 @@ import { formatDate } from "/javascripts/utils.js";
 var league_one_header = `
     <section class="">
         <a href="/first_division" class="bg-gray  row margin-top small-round">
-        <img src="/images/league_1.png" alt="" class="sm-logo margin-left round">
+        <img src="/images/league_1.png" alt="" class="xs-logo margin-left round">
         <h3 class="large bold">Orange First Division League</h3>
         <i class="fa-solid fa-chevron-right move-right margin-right"></i>
         </a>
     </section>
 `
 
-
-
 $(document).ready(function () {
-    $(league_one_header).insertBefore("#league_one");
     $.get("https://www.scorebar.com/api/league//tournament/2315/liberia-second-division",
         function (data, status) {
             let {league, games} = data.result
@@ -28,12 +25,12 @@ $(document).ready(function () {
                         </h3>
                         <section class="column max">
                             <span class="row">
-                                <img src="${item.teams.home.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="sm-logo">
+                                <img src="${item.teams.home.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="xs-logo">
                                 <h3 class="bold max">${item.teams.home.name}</h3>
                                 <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
                             </span>
                             <span class="row">
-                                <img src="${item.teams.away.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="sm-logo">
+                                <img src="${item.teams.away.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="xs-logo">
                                 <h3 class="bold max">${item.teams.away.name}</h3>
                                 <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
                             </span>
@@ -46,3 +43,14 @@ $(document).ready(function () {
         },
     );
 });
+
+
+$(document).on({
+    ajaxStart: function(){
+        $("#loader").show()
+    },
+    ajaxStop: function(){
+        $(league_one_header).insertBefore("#league_one");
+        $("#loader").hide()
+    },
+})
