a3a69e9c40fba1451dcd8dc261188fdcacff03d7
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-17 16:32:56 +0000

    added team squard route handeer

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('team', { title: 'Team' });
 });
+router.get('/squard', function(req, res, next) {
+    res.render('Squard', { title: 'Squard' });
+});
 
 module.exports = router;
