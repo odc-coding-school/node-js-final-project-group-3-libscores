eb437e64dadf1fa99a59ec9c2d6f9b15ef22e724
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-17 16:56:10 +0000

    working on lazy loading

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -17,32 +17,34 @@ const leagueApiMap = {
         let { league, games } = data.result;
         $(".loader").hide();
 
-    // Convert 'games' object into an array and sort by time in descending order
-    const sortedGames = Object.values(games).sort((a, b) => new Date(b.time.start) - new Date(a.time.start));
+      // Convert 'games' object into an array and sort by time in descending order
+      const sortedGames = Object.values(games).sort((a, b) => new Date(b.time.start) - new Date(a.time.start));
 
-    sortedGames.forEach(item => {
-    $(` 
-        <section class="box row padding">
-            <h3 class="${item.status == 3 ? 'black' : 'red'} margin-right column center-align small">
-            <span>${item.status == 3 ? 'FT' : '90'}</span>
-            <span>${formatDate(item.time.start)}</span>
-            </h3>
-            <section class="column max">
-                <span class="row">
-                    <img src="${item.teams.home.logo ? item.teams.home.logo : '/images/shield.webp' }" alt="" class="xs-logo">
-                    <h3 class="bold max">${removeWordFromEnd(item.teams.home.name, "Women")}</h3>
-                    <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
-                </span>
-                <span class="row">
-                    <img src="${item.teams.away.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="xs-logo">
-                    <h3 class="bold max">${removeWordFromEnd(item.teams.away.name, "Women")}</h3>
-                    <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
-                </span>
-            </section>
-        </section>`
-    ).appendTo(`#${divId}`);  // Adjust target as necessary
-    });
+      sortedGames.forEach(item => {
+      $(` 
+          <section class="box row padding">
+              <h3 class="${item.status == 3 ? 'black' : 'red'} margin-right column center-align small">
+              <span>${item.status == 3 ? 'FT' : '90'}</span>
+              <span>${formatDate(item.time.start)}</span>
+              </h3>
+              <section class="column max">
+                  <span class="row">
+                      <img src="${item.teams.home.logo ? item.teams.home.logo : '/images/shield.webp' }" alt="" class="xs-logo">
+                      <h3 class="bold max">${removeWordFromEnd(item.teams.home.name, "Women")}</h3>
+                      <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
+                  </span>
+                  <span class="row">
+                      <img src="${item.teams.away.logo ? item.teams.away.logo : '/images/shield.webp' }" alt="" class="xs-logo">
+                      <h3 class="bold max">${removeWordFromEnd(item.teams.away.name, "Women")}</h3>
+                      <h3 class="${item.status == 3 ? 'black' : 'red'}">${item.score.summary.score1}</h3>
+                  </span>
+              </section>
+          </section>`
+      ).appendTo(`#${divId}`);  // Adjust target as necessary
+      });
+
       },
+
       error: function (err) {
         console.error(`Error fetching data for div ${divId}: `, err);
       }
@@ -90,7 +92,7 @@ const leagueApiMap = {
                     <h3 class="large bold">${league.name}</h3>
                     <i class="fa-solid fa-chevron-right move-right margin-right"></i>
                 </a>
-            </section>`).insertBefore(`#${league.id}`);
+            </section>`).prependTo(`#${league.id}`);
         });
 
         // Hide the loader after the AJAX call completes
