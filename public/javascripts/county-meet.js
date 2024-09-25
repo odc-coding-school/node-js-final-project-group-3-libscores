6c00833ea13026f9800c89863420a89de3ca32b4
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-25 14:42:18 +0000

    added drop down for county meet editions

--- public/javascripts/county-meet.js
+++ public/javascripts/county-meet.js
@@ -4,6 +4,7 @@ import { formatDate, removeWordFromEnd } from "/javascripts/utils.js";
 $(document).ready(function () {
     $.get("/admin/cm/editions/all",
         function (data, textStatus, jqXHR) {
+            alert("cm.js")
             if(textStatus == "success") {
                 data.editions.forEach(edition => {
                     $( `
@@ -17,48 +18,4 @@ $(document).ready(function () {
         },
         "json"
     );
-
-    $("#cmEditions").on("select", function (evt) {
-        let value = evt.target.value()
-        alert(value)
-    });
-
-    $.get("/admin/cm/matches/all",
-        function (data, textStatus, jqXHR) {
-
-            if(textStatus == "success") {
-                
-                $(".loader").hide();
-                let games = data.matches
-                 // Convert 'games' object into an array and sort by time in descending order
-                const sortedGames = Object.values(games).sort((a, b) => new Date(b.match_date) - new Date(a.match_date));
-
-                sortedGames.forEach(match => {
-                    $(
-                    `<section class="box row padding matches">
-                    <h3 class="${'black'} margin-right column center-align small">
-                        <span>${match.start_time}</span>
-                        <span>${formatDate(match.match_date)}</span>
-                    </h3>
-                    <section class="column max cap">
-                        <span class="row">
-                            <img src="/images/shield.webp" alt="" class="sm-logo">
-                            <h3 class="bold max">${match.home_team}</h3>
-                            <h3 class="red">${ new Date(match.match_date) > new Date() ?   match.score_1 : ''}</h3>
-                        </span>
-                        <span class="row">
-                            <img src="/images/shield.webp" alt="" class="sm-logo">
-                            <h3 class="bold max">${match.away_team}</h3>
-                            <h3 class="red">${ new Date(match.match_date) > new Date() ?   match.score_2 : ''}</h3>
-                        </span>
-                    </section>`
-                ).appendTo("#container")})
-            }  else {
-                console.error("An error occurred fetching martches")
-            }
-        },
-        "json"
-    );
-
-
 });
--- routes/county.meet.routes.js
+++ routes/county.meet.routes.js
@@ -1,6 +1,9 @@
 var express = require('express');
-const upload = require('@middleware/upload');
 var router = express.Router();
+const getDbInstance = require('@js/getDBInstance');
+var sqlite3 = require("sqlite3").verbose();
+var db = getDbInstance(sqlite3)
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,9 +22,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:tab', function(req, res, next) {
-
     const {tab} = req.params
-
     let options = { 
         title: 'County Meet', 
         league: "cm", 
@@ -30,23 +31,32 @@ router.get('/:tab', function(req, res, next) {
         tab
      }
 
-    switch (tab) {
-        case "results":
-            res.render('results', options) 
-            break;
-        case "standings":
-            res.render('standings', options) 
-            break;
-        case "fixtures":
-            res.render('fixtures', options) 
-            break;
-        case "statistics":
-            res.render('statistics', options) 
-            break;
-        default:
-            res.render('county-meet', options );
-            break;
-    }
+  db.all("SELECT * FROM editions", function (err, rows) {
+        if(err) { throw new Error(err)} 
+        else { 
+            console.log(rows)
+            options.editions = rows
+            switch (tab) {
+                case "results":
+                    res.render('results', options) 
+                    break;
+                case "standings":
+                    res.render('standings', options) 
+                    break;
+                case "fixtures":
+                    res.render('fixtures', options) 
+                    break;
+                case "statistics":
+                    res.render('statistics', options) 
+                    break;
+                default:
+                    res.render('county-meet', options );
+                    break;
+            }
+        }
+    });
+
+
 
 });
 
