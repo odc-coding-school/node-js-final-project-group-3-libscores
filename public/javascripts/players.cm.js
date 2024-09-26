170776732f9f5e12e3ae6eaff18338b46b2191e6
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-26 08:29:22 +0000

    fetch and display players details on admin dashboard

--- public/javascripts/players.cm.js
+++ public/javascripts/players.cm.js
@@ -5,6 +5,19 @@ $(document).ready(function () {
     $.get("/admin/cm/players/all",
         function (data, textStatus, jqXHR) {
             console.log(data)
+            data.players.forEach(player => {
+                $(`
+                     <section class="row bg-white small-padding small-round align-top">
+                        <img src="/images/${player.photo}" alt="" srcset="" class="mid-img">
+                        <img src="/images/${player.flag}" alt="" srcset="" class="sm-img">
+                        <span class="column no-gap">
+                            <h3 class="bold no-line">${player.first_name} ${player.middle_name} ${player.last_name}</h3>
+                            <small class="mid muted bold">${player.DOB}</small>
+                            <small class="small muted cap">${player.county}</small>
+                        </span>
+                    </section>
+                    `).prependTo("#playerList");
+            });
         },
         "json"
     );
