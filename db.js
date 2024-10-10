881d9a0130c7a20e4e814ea55003e9fff0b4fc5a
Souleymane Dosso <166629341+Smartdosso@users.noreply.github.com>
2024-10-10 00:48:16 +0000

    done with search.ejs functionality remaining design

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Path to the SQLite database file
-const dbPath = path.resolve(__dirname, 'libscores.db'); 
-
-// Create a new database connection
+// Correct path to leagues.db
+const dbPath = path.join(__dirname, 'databases', 'leagues.db');  
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
-        console.log('Connected to the SQLite database.');
+        console.log('Connected to the leagues.db database.');
     }
 });
 
-// Export the database connection
 module.exports = db;
